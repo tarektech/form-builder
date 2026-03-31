86dde2878be1fee45b4226daa27eeca14e7b636a
tarek alzein <tarekalzein97@gmail.com>
2026-03-31 15:08:35 +0300

    Update Next.js configuration to enable output export and set base path from environment variable

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  basePath: process.env.PAGE_BASE_PATH,    
 };
 
 export default nextConfig;
